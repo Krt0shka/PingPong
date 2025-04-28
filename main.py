aabaf58d220ea6770deea63323d3d37afbbb2d9a
Krt0shka <ilia.tatarskikh@gmail.com>
2025-04-28 19:47:36 +0500

    update1 and update2

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ class GameSprite(pygame.sprite.Sprite):
 class Player(GameSprite):
     def update1(self):
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 1:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < 500 - 100:
+            self.rect.y += self.speed
 
     def update2(self):
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 1:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < 500 - 100:
+            self.rect.y += self.speed
